8b23ce627fdd2195ea359bb1123ea506a6c61ce2
Carlitos73490 <71330622+Carlitos73490@users.noreply.github.com>
2023-03-14 12:52:44 +0100

    Delete/Put route working

--- server/api/recipe/[recipeId].ts
+++ server/api/recipe/[recipeId].ts
@@ -1,22 +1,45 @@
-import {defineEventHandler} from "h3";
-import Recipe from "~/classes/Recipe";
-import {readFile} from "fs/promises";
+import {defineEventHandler, getMethod, getRouterParam, readBody} from "h3";
+import {readFile, writeFile} from "fs/promises";
 
 
 export default defineEventHandler(async (event): Promise<RecipeModel | undefined> => {
 
-    if (event.context.params === undefined) {
+const recipeId : string = getRouterParam(event,"recipeId")
+
+if(!recipeId){
+    return
+}
+
+const data : RecipeModel[] = JSON.parse(await readFile("./storage/recipes.json", "utf8"));
+const element : RecipeModel |undefined   =  data.find((recipe)=>{return recipe.title === recipeId})
+
+    if (element === undefined){
         return
     }
 
 
-    const {recipeId} = event.context.params;
-    const data : RecipeModel[] = JSON.parse(await readFile("./storage/recipes.json", "utf8"));
-    const result = data.find((recipe)=>{return recipe.title === recipeId})
+    let index
+switch (getMethod(event)) {
+ case "GET":
+       return element
+case "DELETE":
+        index = data.indexOf(element);
+        if (index > -1) {
+            data.splice(index, 1);
+        }
+    await writeFile("./storage/recipes.json",JSON.stringify(data),"utf8")
+    break;
+case "PUT":
+    // eslint-disable-next-line no-case-declarations
+     const newElement = await readBody(event)
 
-    if (!result){
-        event.node.res.statusCode = 404
-    } else {
-        return result
-    }
+    element.author = newElement.author
+    element.title= newElement.title
+    element.steps= newElement.steps
+    element.ingredients= newElement.ingredients
+
+    await writeFile("./storage/recipes.json",JSON.stringify(data),"utf8")
+    break;
+
+}
 });
