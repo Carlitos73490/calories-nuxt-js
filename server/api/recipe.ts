be8d45d2a59aca432e72b52235b555476a3d1f3e
Carlitos73490 <71330622+Carlitos73490@users.noreply.github.com>
2023-03-14 12:11:07 +0100

    Add route working

--- server/api/recipe.ts
+++ server/api/recipe.ts
@@ -1,17 +1,19 @@
-import {defineEventHandler} from "h3";
+import {defineEventHandler, readBody} from "h3";
 import Recipe from "~/classes/Recipe";
 import * as fs from "fs";
+import {readFile, writeFile} from "fs/promises";
 
-export default defineEventHandler((event): any => {
+export default defineEventHandler(async (event): Promise<any> => {
     // Grab the parameter from the route
-    if(event.context.json === undefined){
-        return
-    }
 
 
-    const recette : Recipe = new Recipe()
 
-    const recipesData = fs.readFileSync('~/storage/recipes.json');
-    const recipesList = recipesData.toJSON()
+    const body = await readBody(event)
+    console.log(body)
+    const data: RecipeModel[] = JSON.parse(await readFile("./storage/recipes.json", "utf8"));
 
+    data.push(body)
+
+    const result = await writeFile("./storage/recipes.json",JSON.stringify(data),"utf8")
+ return result
 });
